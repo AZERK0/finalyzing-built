06ae76286f0f2280aeeb3e341b5d6adb5ac65c18
AZERK0 <raph.chrp@gmail.com>
2024-02-13 01:08:30 +0100

    2.3.1

--- app/directives/ticker-logo.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TickerLogoDirective } from './ticker-logo.directive';
-
-describe('TickerLogoDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TickerLogoDirective();
-    expect(directive).toBeTruthy();
-  });
-});
